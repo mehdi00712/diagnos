02779aa7575cc143a7b92920a8fda5df80419301
mehdi00712 <mbhoyroo246@gmail.com>
2026-05-04 18:06:48 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDsVSa-p1yR47XfkfqpRQI6DdYvVe-BOoM",
@@ -12,6 +20,9 @@ const firebaseConfig = {
   measurementId: "G-ZTZYCL0WM3"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+setPersistence(auth, browserLocalPersistence);
